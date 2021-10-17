4f5efb2041ceea460e6e9d095f198e7cfcde5ca5
fokku <shodusbus@gmail.com>
2021-10-17 14:57:14 +0800

    first commit

---
+++ node_modules/@solana/wallet-adapter-react-ui/lib/WalletModalProvider.d.ts
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+import { WalletModalProps } from './WalletModal';
+export declare const WalletModalProvider: FC<WalletModalProps>;
---
+++ node_modules/@solana/wallet-adapter-react-ui/src/WalletModalProvider.tsx
@@ -0,0 +1,19 @@
+import React, { FC, useState } from 'react';
+import { WalletModalContext } from './useWalletModal';
+import { WalletModal, WalletModalProps } from './WalletModal';
+
+export const WalletModalProvider: FC<WalletModalProps> = ({ children, ...props }) => {
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <WalletModalContext.Provider
+            value={{
+                visible,
+                setVisible,
+            }}
+        >
+            {children}
+            {visible && <WalletModal {...props} />}
+        </WalletModalContext.Provider>
+    );
+};
