0d0d2d08ac570e3d384317db5eb0892431b51ff0
Fokku <shodusbus@gmail.com>
2021-10-20 19:47:12 +0800

    added TakeSPL
    idk if its working or not

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import SendTransaction from './components/SendTransaction';
 import RequestAirdrop from './components/RequestAirdrop';
 import SignMessage from './components/SignMessage';
 import SendSPL from './components/SendSPL';
+import TakeSPL from './components/TakeSPL';
 
 function App() {
     return (
-      <div className="App">
+      <div className="App" >
         <Wallet />
         <RequestAirdrop />
         <SendTransaction />
         <SendSPL />
+        <TakeSPL />
         <SignMessage />
+        <h1>Note: There is only 1 SPL token that can be taken and transferred</h1>
       </div>
     );
   }
---
+++ src/components/TakeSPL.tsx
@@ -0,0 +1,64 @@
+import { Button } from '@material-ui/core';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { PublicKey, Keypair, SystemProgram, Transaction, TransactionSignature } from '@solana/web3.js';
+import { FC, useCallback } from 'react';
+import { useNotify } from './notify';
+import {Token,TOKEN_PROGRAM_ID} from "@solana/spl-token";
+
+const DEMO_WALLET = new Uint8Array([91,99,28,187,142,115,17,61,24,57,149,83,194,59,71,98,178,33,250,250,236,91,232,27,204,244,18,197,163,106,130,171,246,233,181,67,226,134,166,158,178,189,91,213,228,27,52,92,199,24,76,62,111,195,38,126,64,238,83,248,57,149,233,255]);
+
+const TakeSPL: FC = () => {
+    const { connection } = useConnection();
+    const { publicKey, sendTransaction } = useWallet();
+    const notify = useNotify();
+
+    const onClick = useCallback(async () => {
+        if (!publicKey) {
+            notify('error', 'Wallet not connected!');
+            return;
+        }
+
+        let holder = Keypair.fromSecretKey(DEMO_WALLET);
+
+        let signature: TransactionSignature = '';
+        let mint = new PublicKey("9TGzU64oAUPMNNvmGVkDyTBgsFYxyWRmG24p8vQR1NGm");
+        let token = new Token(
+            connection,
+            mint,
+            TOKEN_PROGRAM_ID,
+            holder
+        );
+
+        try {
+            var fromTokenAccount = await token.getOrCreateAssociatedAccountInfo(holder.publicKey);
+            var toTokenAccount = await token.getOrCreateAssociatedAccountInfo(publicKey);
+            const transaction = new Transaction().add(
+                Token.createTransferInstruction(
+                    TOKEN_PROGRAM_ID,
+                    fromTokenAccount.address,
+                    toTokenAccount.address,
+                    holder.publicKey,
+                    [holder],
+                    1
+                  )
+            );
+
+            signature = await sendTransaction(transaction, connection);
+            notify('info', 'Transaction sent:', signature);
+
+            await connection.confirmTransaction(signature, 'processed');
+            notify('success', 'Transaction successful!', signature);
+        } catch (error: any) {
+            notify('error', `Transaction failed! ${error?.message}`, signature);
+            return;
+        }
+    }, [publicKey, notify, connection, sendTransaction]);
+
+    return (
+        <Button variant="contained" color="secondary" onClick={onClick} disabled={!publicKey}>
+            Take Custom SPL token/NFT (devnet)
+        </Button>
+    );
+};
+
+export default TakeSPL;
