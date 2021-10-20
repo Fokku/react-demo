a73353c5720d61555748c566d3d2659bb6c04fd1
Fokku <shodusbus@gmail.com>
2021-10-20 12:47:53 +0800

    removing node_modules

--- node_modules/@solana/wallet-adapter-react-ui/README.md
+++
@@ -1,5 +0,0 @@
-# `@solana/wallet-adapter-ui`
-
-<!-- @TODO -->
-
-Coming soon.
--- node_modules/@solana/wallet-adapter-react-ui/lib/useWalletModal.js
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from 'react';
-export const WalletModalContext = createContext({});
-export function useWalletModal() {
-    return useContext(WalletModalContext);
-}
-//# sourceMappingURL=useWalletModal.js.map
